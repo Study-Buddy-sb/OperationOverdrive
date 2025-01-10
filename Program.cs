0ef76fb0cba9068b1df6a6dd5750f10c1839657b
balogun14 <awwalbalogun06@gmail.com>
2025-01-10 22:46:58 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿// operators 
+
+/*
+Arithmetic operators – they are used to perform simple mathematical operations.
+casting 
+we have two types of casting 
+implicit and explicit
+examples of mathimatical operation we have
+
+Addition
+numerical datatypes
+byte
+integer  -- 4bytes
+long
+float
+double
+decimal
+
+Subtraction
+Division
+Multiplication
+Modulo
+Exponetiation
+
+- Assignment operators – allow assigning values to variables.
+- Comparison operators – allow comparison of two literals and/or
+variables.
+- Logical operators – operators that work with Boolean data types and
+Boolean expressions.
+BitWise Operators
+- Type conversion operators – allow conversion of data from one type to another
+*/
+
+
+
+// int smallNumber = 10;
+// long sameNumber = smallNumber;
+
+// long someNumber = 1000000000000000;
+// int number = (int)someNumber;
+
+
+
+// int result = 100 + 20;
+// Console.WriteLine(result);
+
+// int result = 100 - 20;
+// Console.WriteLine(result);
+
+// int result = 100 / 20;
+// Console.WriteLine(result);
+
+// int result = 100 * 20;
+// Console.WriteLine(result);
+
+// double result = (100 + 20) - 50.0 * 34.0 / 15.0;
+// Console.WriteLine(result);
+
+// int result = 100 % 21;
+// Console.WriteLine(result);
+
+// double result = Math.Pow(2,5);
+// Console.WriteLine(result);
+
+// int result = 100;
+// Console.WriteLine(result);
+// result += 20;   // result = result + 20
+// Console.WriteLine(result);
+
+// int result = 100;
+// Console.WriteLine(result);
+// result %= 20;   // result = result + 20
+// Console.WriteLine(result);
+
+
+// Console.WriteLine(5 != 6);
+
+bool isGreaterThan = 5 > 6;  //false
+
+bool isLessThan = 6 < 12;  // true
+
+// Console.WriteLine(isGreaterThan && isLessThan);
+
